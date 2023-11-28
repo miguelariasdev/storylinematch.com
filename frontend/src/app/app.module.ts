012a5a602bad6fce8cf915c7fa7011c794631010
miguelariasdev <73453206+miguelariasdev@users.noreply.github.com>
2023-11-28 15:04:53 +0100

    ignore

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './pages/home/home.component';
+/* import { HomeComponent } from './pages/home/home.component'; */
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
